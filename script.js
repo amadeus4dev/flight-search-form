defe6e248781e57a3f88ab86beeafb28d739c403
Anna Tsolakou <anna.tsolakou@amadeus.com>
2023-02-08 14:21:51 +0100

    add validation

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const form = document.getElementById("flight-form");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  const origin = form.elements.origin.value;
-  const destination = form.elements.destination.value;
-  const departureDate = form.elements.departureDate.value;
-  const returnDate = form.elements.returnDate.value;
-
-});
+function validateForm() {
+  var adults = document.getElementById("adults").value;
+  var children = document.getElementById("children").value;
+  var infants = document.getElementById("infants").value;
+  var infantsError = document.getElementById("infantsError");
+  if (parseInt(infants) > parseInt(adults)) {
+      infantsError.innerHTML = "Number of infants cannot exceed number of adults";
+      return false;
+  }
+  console.log(children + infants + adults)
+  if ((parseInt(children) + parseInt(infants) + parseInt(adults)) > 18) {
+      infantsError.innerHTML = "Number of passengers can't exceed 18";
+      return false;
+  }
+  infantsError.innerHTML = "";
+  return true;
+}
