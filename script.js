28256736f99f84a80a4625b330512b1dae121763
Anna Tsolakou <anna.tsolakou@amadeus.com>
2023-02-09 14:42:06 +0100

    remove unused condition

--- script.js
+++ script.js
@@ -3,31 +3,24 @@ function validateForm() {
   var children = document.getElementById("children").value;
   var infants = document.getElementById("infants").value;
   var infantsError = document.getElementById("infantsError");
-  if (parseInt(infants) > parseInt(adults)) {
-      infantsError.innerHTML = "Number of infants cannot exceed number of adults";
-      return false;
-  }
-  console.log(children + infants + adults)
   if ((parseInt(children) + parseInt(infants) + parseInt(adults)) > 18) {
-      infantsError.innerHTML = "Number of passengers can't exceed 18";
-      return false;
+    infantsError.innerHTML = "Number of passengers can't exceed 18";
+    return false;
   }
   infantsError.innerHTML = "";
   return true;
 }
 
-function dynamicDropDown(listIndex)
-{
+function dynamicDropDown(listIndex) {
 
   document.getElementById("infants").length = 0;
   document.getElementById("children").length = 0;
 
-  for (let i = 0; i < Number(listIndex) + 1; i++){
-    document.getElementById("infants").options[i]=new Option(i.toString(),i);
+  for (let i = 0; i < Number(listIndex) + 1; i++) {
+    document.getElementById("infants").options[i] = new Option(i.toString(), i);
   }
 
-  for (let i = 0; i < 9 - Number(listIndex) + 1; i++){
-    document.getElementById("children").options[i]=new Option(i.toString(),i);
+  for (let i = 0; i < 9 - Number(listIndex) + 1; i++) {
+    document.getElementById("children").options[i] = new Option(i.toString(), i);
   }
-
 }
